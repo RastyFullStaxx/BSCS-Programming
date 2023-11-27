82e18d454a3ccd7a64c093cdb8df3c8c0fd9eb61
Rasty Espartero <gemrasty@gmail.com>
2023-11-28 04:57:28 +0800

    Create Nodes.java

---
+++ Second Year/Linked List/Nodes.java
@@ -0,0 +1,15 @@
+// Node Class
+public class Nodes {
+
+    // [1] Declare Variables
+    int data;
+
+    // [2] Declared a mode reference for my singly linked list (since I only have next)
+    Nodes next;
+
+    // [3] Created a constructor for int data
+    Nodes(int data) {
+        this.data = data;
+    }
+
+}
