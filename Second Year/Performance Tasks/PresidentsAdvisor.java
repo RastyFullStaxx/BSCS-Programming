19c84a5210d6cb5c50f3073dc8b094ef66a9d1da
Rasty Espartero <gemrasty@gmail.com>
2023-12-04 23:05:42 +0800

    Update PresidentsAdvisor.java

--- Second Year/Performance Tasks/PresidentsAdvisor.java
+++ Second Year/Performance Tasks/PresidentsAdvisor.java
@@ -1,71 +1,106 @@
-package Java.DataStrucAlgo.Stacks.PerformanceTask2;
+/*  COMP 006: DATA STRUCTURES AND ALGORITHMS
+ *  PROGRAMMING TASK 3 - CASE STUDY: PRESIDENT'S ADVISORS
+ *  Leader:          Rasty Espartero
+ *  Contributors:    David Geisler Mahayag       Marc Lorenz Quintana
+ *                   Jhanice Pablo               Marian Facunla
+ *                   Jose Mari Aguinaldo         Roanne Maye Dinglasa 
+ * 
+ *       USING LINKEDLIST (Tried to mimic arraylist performance)        */
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class PresidentsAdvisor {
 
+    // [1] Main method where the program execution starts
     public static void main(String[] args) {
+
+        // [2] Instantiate the scanner object to accept user input
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            // [1] Input: Get starting advisor from the user
-            System.out.print("Start: ");
-            int startAdvisor = scanner.nextInt();
+        // [3] Display initial message
+        lineBreak();
+        System.out.println("Programming Task 3: Case Problem\n\tPresident's Advisors");
 
-            // [2] Check if the user wants to exit (0 indicates exit)
-            if (startAdvisor == 0) {
-                break;
-            }
+        outerLoop: while (true) {
 
-            // [3] Input: Get counting interval from the user
-            System.out.print("Interval: ");
-            int countingInterval = scanner.nextInt();
+            // [5] Input: Get starting adviser from the user with validation
+            int startAdviser;
 
-            // [4] Advisors: Initialize a list to represent the advisors (1 to 13)
-            List<Integer> advisorsList = new ArrayList<>();
+            do {
+                lineBreak();
+                System.out.print("Start: \t\t");
+                startAdviser = scanner.nextInt();
+                if (startAdviser > 13) {
+                    System.out.println("\n   Error: Invalid Start Adviser\n    Enter value between 1 to 13 \n\t    or 0 to exit\n\t  * * * * * * * *\n");
+                } else if (startAdviser == 0) {
+                    System.out.println("\nExiting..\n");
+                    break outerLoop; // Use labeled break to exit the outer loop
+                }
+            } while (startAdviser < 1 || startAdviser > 13);
+
+            // [7] Input: Get counting interval from the user with validation
+            int interval;
+            do {
+                System.out.print("Interval: \t");
+                interval = scanner.nextInt();
+                if (interval > 100) {
+                    System.out.println("\n     Error: Invalid interval\n   Enter value between 1 to 100\n\t  * * * * * * * *\n");
+                }
+            } while (interval > 100);
+
+            // [8] Store Integers 1 to 13 to the queues using for loop
+            Queue<Integer> advisers = new LinkedList<>();
             for (int i = 1; i <= 13; i++) {
-                advisorsList.add(i);
+                advisers.offer(i);
             }
 
-            // [5] Select Advisors: Call the method to select advisors
-            List<Integer> selectedAdvisors = selectAdvisors(advisorsList, startAdvisor, countingInterval);
+            // [9] Select advisers: Call method and pass on the arguments
+            Queue<Integer> chosenAdvisers = selectAdvisers(advisers, startAdviser, interval);
 
-            // [6] Output: Display the selected advisors
-            System.out.print("Selected Advisors: ");
-            for (int i = 0; i < selectedAdvisors.size(); i++) {
-                System.out.print(selectedAdvisors.get(i));
+            // [10] Output: Display the selected advisers
+            System.out.print("Chosen Advisers: ");
+            while (!chosenAdvisers.isEmpty()) {
+                System.out.print(chosenAdvisers.poll());
 
-                // Add a comma and space if it's not the last advisor
-                if (i < selectedAdvisors.size() - 1) {
+                // [11] Add a comma and space if it's not the last adviser
+                if (!chosenAdvisers.isEmpty()) {
                     System.out.print(", ");
                 }
             }
-            System.out.println("\n"); // Add a new line for better formatting
+            System.out.println(""); // [12] Add a new line for better formatting
         }
 
-        // [7] Close the Scanner to prevent resource leaks
-        scanner.close();
+        scanner.close(); // [13] Close the Scanner outside the loop
     }
 
-    // [8] Method to select advisors, skipping those already counted
-    private static List<Integer> selectAdvisors(List<Integer> advisors, int startAdvisor, int countingInterval) {
-        List<Integer> selectedAdvisors = new ArrayList<>();
-        int currentIndex = startAdvisor - 1; // Adjust for 0-based indexing
+    // [14] Method to select advisers, skipping those already counted
+    private static Queue<Integer> selectAdvisers(Queue<Integer> advisers, int startAdviser, int interval) {
 
-        // [9] Loop to select advisors based on the counting interval
-        while (selectedAdvisors.size() < 5) {
-            // [10] Ensure the current index is within the bounds of the advisors list
-            currentIndex %= advisors.size();
+        // [15] Initialize a queue to store the selected advisers
+        Queue<Integer> chosenAdvisers = new LinkedList<>();
 
-            int currentAdvisor = advisors.remove(currentIndex);
-            selectedAdvisors.add(currentAdvisor);
+        // [16] Adjust for 0-based indexing
+        startAdviser--;
 
-            // [11] Update the current index for the next iteration
-            currentIndex += countingInterval - 1;
+        // [17] Loop to select advisers based on the counting interval
+        while (chosenAdvisers.size() < 5) {
+
+            // [18] Skip advisers who have already been counted
+            for (int i = 1; i < interval; i++) {
+                advisers.offer(advisers.poll());
+            }
+
+            // [19] Add the current adviser to the selected list
+            chosenAdvisers.offer(advisers.poll());
         }
 
-        return selectedAdvisors;
+        return chosenAdvisers; // [20] Return the selected advisers
+    }
+
+    // [21] Utility method to print a line break
+    static void lineBreak() {
+        System.out.println("=================================");
     }
 }
